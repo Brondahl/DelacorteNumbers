e6369be37e71cb3c522cc0cafa52249fda6ba277
Brondahl <stixmclean@googlemail.com>
2014-10-05 12:57:14 +0100

    More programs to run

--- DelacorteNumbers/Program.cs
+++ DelacorteNumbers/Program.cs
@@ -12,13 +12,33 @@ namespace DelacorteNumbers
     {
         static void Main()
         {
+            New();
+        }
+
+        private static void New()
+        {
+            var arrayInput = new int[3,3] {{2, 3, 4}, {9,1,5}, {8,7,6}};
+            new DelacorteGrid(arrayInput);
+            new DelacorteGridEvaluator(arrayInput).BreakDown();
+        }
+
+        static void ExhSearch3Limited()
+        {
             var x = new Stopwatch();
             x.Start();
             new ExhaustiveSearch(3, 180, 125).Run();
             x.Stop();
             Console.WriteLine("Completed");
             Console.WriteLine(x.ElapsedMilliseconds);
-            Thread.Sleep(10000);
+        }
+        static void ExhSearchSimple(int n)
+        {
+            var x = new Stopwatch();
+            x.Start();
+            new ExhaustiveSearch(n).Run();
+            x.Stop();
+            Console.WriteLine("Completed");
+            Console.WriteLine(x.ElapsedMilliseconds);
         }
     }
 }
