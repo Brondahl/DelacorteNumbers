146d3342742278efdf90125ca3e86a02735253fa
Brondahl <stixmclean@googlemail.com>
2014-10-06 09:43:17 +0100

    Improve Permutation Generator performance

--- DelacorteNumbers/NumberPermuter.cs
+++ DelacorteNumbers/NumberPermuter.cs
@@ -10,7 +10,7 @@ namespace DelacorteNumbers
         {
             var numbers = Enumerable.Range(1, n).ToList();
 
-            return RecursivePermute(numbers);
+            return RecursivePermuteWithDepth(valuesToPermute, valuesToPermute.Count);
         }
 
         private static IEnumerable<IEnumerable<int>> RecursivePermute(List<int> input)
@@ -25,12 +25,34 @@ namespace DelacorteNumbers
             {
                 var output = new List<int>();
                 output.Add(selected);
-                var inputMinusSelected = input.Except(new List<int> {selected}).ToList();
+                var inputMinusSelected = input.Except(new List<int> { selected }).ToList();
                 foreach (var subPermute in RecursivePermute(inputMinusSelected))
                 {
                     yield return output.Concat(subPermute);
                 }
             }
         }
+
+        private static IEnumerable<IEnumerable<int>> RecursivePermuteWithDepth(List<int> input, int depth)
+        {
+            if (depth == 1)
+            {
+                yield return input;
+                yield break;
+            }
+
+            for (int i = 0; i < depth; i++)
+            {
+                var output = new List<int>();
+                output.Add(input[i]);
+                var inputCopy = input.ToList();
+                inputCopy.RemoveAt(i);
+                foreach (var subPermute in RecursivePermuteWithDepth(inputCopy, depth - 1))
+                {
+                    yield return output.Concat(subPermute);
+                }
+            }
+        }
+
     }
 }
